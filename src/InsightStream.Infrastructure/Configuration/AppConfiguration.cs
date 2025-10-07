b3a06ab388cd1c0026043c3a93cae2313b5f26d1
Stefan <sjslagter@hotmail.com>
2025-10-07 18:04:30 +0200

    fixed AppConfiguration issue

--- src/InsightStream.Infrastructure/Configuration/AppConfiguration.cs
+++ src/InsightStream.Infrastructure/Configuration/AppConfiguration.cs
@@ -2,7 +2,7 @@ namespace InsightStream.Infrastructure.Configuration;
 
 public sealed class AppConfiguration
 {
-    public const string SectionName = "DefaultProvider";
+    public const string SectionName = "AppConfiguration";
     
     public required string DefaultProvider { get; init; }
 }
